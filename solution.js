d1ee21e3078823b5b416e9f89eca02072f2a04b9
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-06-29 13:31:58 +0200

    exercises 1

--- solution.js
+++ solution.js
@@ -1,1 +1,17 @@
 // write your code in this file
+
+console.log(`This is an exercise in console logging`);
+const exercises = "This is the value."
+console.log(exercises);
+
+let firstName = "Izabela";
+let lastName = "Zuchowska";
+console.log(firstName, lastName);
+
+let age = 35;
+let city = "Berlin";
+let job = "Unemployed";
+console.log(firstName, lastName, age, city, job)
+
+console.log(firstName, lastName,`is a`, age,  `years old`, job, `who lived in`, city);
+
